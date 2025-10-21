8b28b9efef35f8d861d0c2a075b979850b226eb2
Berneos <brenomartins880@gmail.com>
2025-10-21 20:35:50 -0300

    Commit inicial

---
+++ src/main/java/com/pitufos/absolutemovies/AbsolutemoviesApplication.java
@@ -0,0 +1,13 @@
+package com.pitufos.absolutemovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsolutemoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsolutemoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitufos/absolutemovies/AbsolutemoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitufos.absolutemovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsolutemoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
